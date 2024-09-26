b327e57f549fb66a6cd52dc6307fa83fe631201c
Arnav-Sangle <arnavsangle7x24@gmail.com>
2024-09-26 11:27:32 +0530

    Refined with branding (PS: Previous was testing)
    Output display was corrected
    Comments were replaced with own

--- CustomCipher.java
+++ CustomCipher.java
@@ -1,102 +1,98 @@
-class CustomCipher 
-{
+import java.util.*;
 
-// This function generates the key in
-// a cyclic manner until it's length isi'nt
-// equal to the length of original text
-static String generateKey(String str, String key)
-{
-    int x = str.length();
-
-    for (int i = 0; ; i++)
+class CustomCipher {
+    // This function repeats the key in a cyclic manner to match the length of the Plaintext
+    static String generateKey(String str, String key)
     {
-        if (x == i)
-            i = 0;
-        if (key.length() == str.length())
-            break;
-        key+=(key.charAt(i));
-    }
-    return key;
-}
-
-// This function returns the encrypted text
-// generated with the help of the key
-static String cipherText(String str, String key)
-{
-    String cipher_text="";
-    int arr[] = new int[str.length()];
+        int x = str.length();
     
-    
-    for (int i = 0; i < str.length(); i++)
-    {
-        // converting in range 0-25
-        int x = ((str.charAt(i)-'A') ^ (key.charAt(i)-'A'));
-
-        // Converting to int to char
-        x += 'A';
-        cipher_text+=(char)(x);
-        
-        
-    }
-    return cipher_text;
-}
-
-// This function decrypts the encrypted text
-// and returns the original text
-static String originalText(String cipher_text, String key)
-{
-    String orig_text="";
-
-    for (int i = 0 ; i < cipher_text.length() && 
-                            i < key.length(); i++)
-    {
-        // converting in range 0-25
-        //  int x = (cipher_text.charAt(i));
-        int x = ((cipher_text.charAt(i)-'A') ^ (key.charAt(i)-'A'));
-
-        // convert into alphabets(ASCII)
-        x += 'A';
-        orig_text+=(char)(x);
-    }
-    return orig_text;
-}
-
-// This function will convert the lower case character to Upper case
-static String LowerToUpper(String s)
-{
-    StringBuffer str =new StringBuffer(s); 
-    for(int i = 0; i < s.length(); i++)
-    {
-        if(Character.isLowerCase(s.charAt(i)))
+        for (int i = 0; ; i++)
         {
-            str.setCharAt(i, Character.toUpperCase(s.charAt(i)));
+            if (x == i)
+                i = 0;
+            if (key.length() == str.length())
+                break;
+            key+=(key.charAt(i));
         }
+        return key;
     }
-    s = str.toString();
-    return s;
-}
-
-// Driver code
-public static void main(String[] args) 
-{
-    // String Str = "GEEKSFORGEEKS";
-    // String Keyword = "AYUSH";
-    String Str = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    String Keyword = "";
-     
-     for(int i=13; i<26; i++) {
-          Keyword = "";
-        String str = LowerToUpper(Str);
-        String keyword = LowerToUpper(Keyword + str.charAt(i));
-
-
-        String key = generateKey(str, keyword);
-        String cipher_text = cipherText(str, key);
     
-        System.out.print("i: " + i + ",  Keyword: " + keyword + "\n");
-        System.out.println("C: " + cipher_text);
-        System.out.println("P: " + originalText(cipher_text, key) + "\n");
+    // Encryption
+    static String Encrypt(String str, String key)
+    {
+        String ciphertext="";
+        int arr[] = new int[str.length()];
         
+        
+        for (int i = 0; i < str.length(); i++)
+        {
+            // Encryption formula
+            int x = ((str.charAt(i)-'A') ^ (key.charAt(i)-'A'));
+            
+            // convert into alphabets(ASCII) and add to string
+            x += 'A';                  
+            ciphertext+=(char)(x);
         }
+        return ciphertext;
+    }
+    
+    // Decryption
+    static String Decrypt(String ciphertext, String key)
+    {
+        String plaintext="";
+    
+        for (int i = 0 ; i < ciphertext.length() && 
+                                i < key.length(); i++)
+        {
+             // Decryption formula
+            int x = ((ciphertext.charAt(i)-'A') ^ (key.charAt(i)-'A'));
+    
+            // convert into alphabets(ASCII) and add to string
+            x += 'A';
+            plaintext+=(char)(x);
+        }
+        return plaintext;
+    }
+    
+    static String LowerToUpper(String s)
+    {
+        StringBuffer str =new StringBuffer(s); 
+        for(int i = 0; i < s.length(); i++)
+        {
+            if(Character.isLowerCase(s.charAt(i)))
+            {
+                str.setCharAt(i, Character.toUpperCase(s.charAt(i)));
+            }
+        }
+        s = str.toString();
+        return s;
+    }
+    
+    
+    
+    // Main
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+            
+        System.out.print("\n Enter PlainText - ");
+    	String Str = sc.nextLine();
+    	
+    	System.out.print(" Enter Keyword   - ");
+    	String Keyword = sc.nextLine();
+    	
+        // String Str = "This is a Secret Message";
+        // String Keyword = "Secret";
+         
+        String str = LowerToUpper(Str);
+        String keyword = LowerToUpper(Keyword);
+    
+        String key = generateKey(str, keyword);
+        String cipherText = Encrypt(str, key);
+    
+        System.out.println("\n Encrypted text  - " + cipherText);
+        System.out.println(" Decrypted text  - " + Decrypt(cipherText, key));
+            
+        sc.close();
+            
     }
 }
